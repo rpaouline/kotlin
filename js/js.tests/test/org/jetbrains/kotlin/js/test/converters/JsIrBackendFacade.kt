a605ca9dfc1fdff8cecabdc582f960c0083833c7
Alexander Korepanov <alexander.korepanov@jetbrains.com>
2022-10-28 11:03:14 +0200

    [JS IR] More detailed time measurement for the IC infrastructure

--- js/js.tests/test/org/jetbrains/kotlin/js/test/converters/JsIrBackendFacade.kt
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/converters/JsIrBackendFacade.kt
@@ -108,7 +108,7 @@ class JsIrBackendFacade(
                         caches = testServices.jsIrIncrementalDataProvider.getCaches(),
                         relativeRequirePath = false
                     )
-                    jsExecutableProducer.buildExecutable(it.perModule, true)
+                    jsExecutableProducer.buildExecutable(it.perModule, true).compilationOut
                 },
                 tsDefinitions = null
             )
